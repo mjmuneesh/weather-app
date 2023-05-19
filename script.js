3c7f5f94eb2ab06496e74ff728badf589fe09bc7
mj_muneesh <muneeshsharma094@gmail.com>
2023-05-19 12:01:52 +0530

    weather app using async and await

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let defaultCity="jammu";
+
+let defaultCity = "jammu";
 const input = document.getElementById("input");
 const submitBtn = document.getElementById("submit");
 
@@ -7,31 +8,35 @@ const temp = document.getElementById("temp");
 const humidity = document.getElementById("humidity");
 
 function getWeather(namee) {
-  fetch( `https://api.openweathermap.org/data/2.5/weather?q=${namee}&appid=dda26109be2e21c827819fa96320454e&units=metric`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("City not found.");
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data.name);
+  async function fetchWeather() {
+
+    let fetchData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${namee}&appid=dda26109be2e21c827819fa96320454e&units=metric`)
+    let data = await fetchData.json();
+    return data;
+  }
+
+  fetchWeather().then(data => {
+    if (data.name == undefined) {
+      cityName.textContent = data.message;
+      temp.textContent = `Temperature: `;
+      humidity.textContent = `Humidity: `;
+      wind.textContent = `Wind Speed: `;
+    } else {
       cityName.textContent = data.name;
       temp.textContent = `Temperature: ${data.main.temp}°C`;
       humidity.textContent = `Humidity: ${data.main.humidity}%`;
       wind.textContent = `Wind Speed: ${data.wind.speed} m/s`;
-    })
-    .catch(error =>{
-        console.error(error);
-    })
+    }
+
+  })
 }
 
 getWeather(defaultCity);
 
 submitBtn.addEventListener('click', () => {
-    const city = input.value.trim();
-    if (city) {
-        getWeather(city);
-        input.value = '';
-    }
+  const city = input.value.trim();
+  if (city) {
+    getWeather(city);
+    input.value = '';
+  }
 });
